9194374770334a26d445058e94eae64ebb3b8348
SergeyHv <skhvostenok@yandex.ru>
2025-12-19 15:21:48 +0300

    Update products.js

--- api/products.js
+++ api/products.js
@@ -1,8 +1,8 @@
-import { GoogleSpreadsheet } from 'google-spreadsheet';
+const { GoogleSpreadsheet } = require('google-spreadsheet');
 
-export default async function handler(req, res) {
+module.exports = async (req, res) => {
   try {
-    const doc = new GoogleSpreadsheet(process.env.GOOGLE_SHEETS_SPREADSHEET_ID);
+    const doc = new GoogleSpreadsheet(process.env.GOOGLE_SHEETS_ID);
 
     await doc.useServiceAccountAuth({
       client_email: process.env.GOOGLE_SHEETS_CLIENT_EMAIL,
@@ -14,19 +14,14 @@ export default async function handler(req, res) {
     const rows = await sheet.getRows();
 
     const products = rows.map(r => ({
-      id: r.get('id'),
-      title: r.get('title'),
-      price: Number(r.get('price')),
-      images: r.get('images'),
-      category: r.get('category'),
-      tags: r.get('tags'),
-      description: r.get('description'),
-      stock: String(r.get('stock')).toLowerCase() === 'true',
-      props: r.get('props'),
+      name: r.Name,
+      description: r.Description,
+      image: r.Image,
     }));
 
-    res.status(200).json({ items: products });
+    res.status(200).json(products);
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
-}
+};
+
