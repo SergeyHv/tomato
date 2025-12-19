f5590c02642ef8daa71fd68581d8e35458000216
SergeyHv <skhvostenok@yandex.ru>
2025-12-19 15:18:24 +0300

    Update test.js

--- api/test.js
+++ api/test.js
@@ -1,9 +1,3 @@
-// test route check 
 export default function handler(req, res) {
-  res.status(200).json({
-    spreadsheetId: process.env.GOOGLE_SHEETS_SPREADSHEET_ID,
-    clientEmail: process.env.GOOGLE_SHEETS_CLIENT_EMAIL,
-    privateKeyStart: process.env.GOOGLE_SHEETS_PRIVATE_KEY?.substring(0, 50)
-  });
+  res.status(200).json({ message: "API test route works!" });
 }
-
