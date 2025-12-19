fa1ef74c6182cb1f52eb3749385e9df0a6696595
SergeyHv <skhvostenok@yandex.ru>
2025-12-19 14:47:34 +0300

    Create test.js

---
+++ pages/api/test.js
@@ -0,0 +1,7 @@
+export default function handler(req, res) {
+  res.status(200).json({
+    spreadsheetId: process.env.GOOGLE_SHEETS_SPREADSHEET_ID,
+    clientEmail: process.env.GOOGLE_SHEETS_CLIENT_EMAIL,
+    privateKeyStart: process.env.GOOGLE_SHEETS_PRIVATE_KEY?.substring(0, 50)
+  });
+}
