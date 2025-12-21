96e42fd94a51668071605238bb0cc61fe08a3159
SergeyHv <skhvostenok@yandex.ru>
2025-12-21 20:59:55 +0300

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import Header from "./Header";
+import Header from "./components/Header";
 import ProductGrid from "./ProductGrid";
 
 export default function App() {
