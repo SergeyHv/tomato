78c7b17a2aca2faab95446815f055c443dc734fc
SergeyHv <skhvostenok@yandex.ru>
2026-05-17 16:15:29 +0300

    Update Catalog.tsx

--- src/components/Catalog.tsx
+++ src/components/Catalog.tsx
@@ -430,7 +430,7 @@ export const Catalog: React.FC<CatalogProps> = ({
         <div ref={topAnchorRef} className="sr-only" aria-hidden />
 
         {/* Мобильный sticky-блок с улучшенным позиционированием */}
-       <div className="sticky top-20 z-30 bg-white pt-2 pb-2 lg:hidden shadow-sm"> Раскройте блок фильтров(три полоски) или впишите в строку поиска слово(или часть), например, сладк
+       <div className="sticky top-20 z-30 bg-white pt-2 pb-2 lg:hidden shadow-sm"> Раскройте блок фильтров(три полоски) или впишите в строку поиска слово(или часть), например, сладк или пальчик
           <div className="flex items-center gap-2">
             <button
               onClick={toggleFilters}
